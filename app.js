e6e6faca5ccbb27338e738ffca92df5014a89c06
Coscle <newsrain@naver.com>
2024-05-27 14:16:05 +0900

    remove method

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.end('<h1>Hello world!</h1>')
 });
 
-app.get('/', (req, res) => {
-    res.end('<h1>Hello world2!</h1>')
-});
-
-app.get('/shop', (req, res) => {
-    res.end('<h1>Hello world2!</h1>')
-});
 
 const server = http.createServer(app);
 server.listen(3000, ()=> {
