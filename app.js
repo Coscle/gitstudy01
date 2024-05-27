bfd122186872354cbe00519e5d3a2930466d5e32
Coscle <newsrain@naver.com>
2024-05-27 14:13:26 +0900

    step1

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.end('<h1>Hello world2!</h1>')
 });
 
+app.get('/shop', (req, res) => {
+    res.end('<h1>Hello world2!</h1>')
+});
+
 const server = http.createServer(app);
 server.listen(3000, ()=> {
     console.log('Run on server http://localhost:3000');
