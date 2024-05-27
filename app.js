be4f9b97a1fbf27e4acd2655f0041f8a60e305c3
Coscle <newsrain@naver.com>
2024-05-27 13:59:45 +0900

    branch commit

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ app.get('/', (req, res) => {
     res.end('<h1>Hello world!</h1>')
 });
 
+app.get('/', (req, res) => {
+    res.end('<h1>Hello world2!</h1>')
+});
+
 const server = http.createServer(app);
 server.listen(3000, ()=> {
     console.log('Run on server http://localhost:3000');
-});
+});git
